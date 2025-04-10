e7333d26c1af4720896d89bfd98ebf647554b723
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-04-11 00:37:30 +0200

    Update expo-env.d.ts

--- client-mobile/expo-env.d.ts
+++ client-mobile/expo-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="expo/types" />
-//EXPO_PUBLIC_API_URL=https://barber-dev-server.vercel.app
+//EXPO_PUBLIC_API_URL=https://barber-development-server.vercel.app
 // NOTE: This file should not be edited and should be in your git ignore
