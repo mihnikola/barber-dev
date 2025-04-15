772a34cd8a8a5b8f58e8577896d63216cffe4393
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-04-15 16:31:05 +0200

    Update DateTimeReservation.tsx

--- client_new/app/components/reservation/DateTimeReservation.tsx
+++ client_new/app/components/reservation/DateTimeReservation.tsx
@@ -1,27 +1,6 @@
-import { getStorage } from "@/helpers";
-import { useEffect, useState } from "react";
 import DateComponent from "./DateComponent";
-import SignForm from "../SignForm/SignForm";
-
 const DateTimeReservation = () => {
-  //  const [isLoggedIn, setIsLoggedIn] = useState(null);
-
-  //    useEffect(() => {
-  //      getStorage()
-  //        .then((res) => {
-  //          if (res) {
-  //            setIsLoggedIn(res);
-  //          } else {
-  //            setIsLoggedIn(null)
-  //          }
-  //        })
-  //        .catch((error) => console.log("error", error));
-  //    }, []);
-
-  return (
-    // <>{isLoggedIn ? <DateComponent /> : <SignForm />}</>
-    <DateComponent />
-  );
+  return <DateComponent />;
 };
 
 export default DateTimeReservation;
